b25d955ce91719d21d94a9bb7660e42e297b3266
youngjun <youngjun@192.168.0.198>
2016-07-21 21:01:54 +0900

    6번 한글 깨짐 해결

--- src/main/java/core/web/filter/CharacterEncodingFilter.java
+++ src/main/java/core/web/filter/CharacterEncodingFilter.java
@@ -8,7 +8,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 
+@WebFilter(filterName="Set Character Encoding",urlPatterns="/*",initParams={@WebInitParam(name="encoding",value="UTF-8")})  
 public class CharacterEncodingFilter implements Filter {
 	private static final String DEFAULT_ENCODING = "UTF-8";
 
