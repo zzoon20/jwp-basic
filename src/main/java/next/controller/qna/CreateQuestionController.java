77772030ba82019668011c3d8cec80237499007f
youngjun <youngjun@192.168.0.198>
2016-07-21 20:27:20 +0900

    CreateQuestionController init

---
+++ src/main/java/next/controller/qna/CreateQuestionController.java
@@ -0,0 +1,17 @@
+package next.controller.qna;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.mvc.AbstractController;
+import core.mvc.ModelAndView;
+
+public class CreateQuestionController extends AbstractController {
+
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
