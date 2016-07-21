d90bfae6bfce3d2855a5545492b526070d534b0e
youngjun <youngjun@192.168.0.198>
2016-07-21 20:37:19 +0900

    3번 질문하기 구현

--- src/main/java/next/controller/qna/CreateQuestionController.java
+++ src/main/java/next/controller/qna/CreateQuestionController.java
@@ -3,15 +3,26 @@ package next.controller.qna;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import core.mvc.AbstractController;
+import core.mvc.JspView;
 import core.mvc.ModelAndView;
+import next.dao.QuestionDao;
+import next.model.Question;
 
 public class CreateQuestionController extends AbstractController {
-
+	private static final Logger logger = LoggerFactory.getLogger(CreateQuestionController.class);
+	
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		Question question = new Question(request.getParameter("writer"), request.getParameter("title"),
+				request.getParameter("contents"));
+		logger.debug("question = {}", question.toString());
+		QuestionDao qd = new QuestionDao();
+		qd.insert(question);
+		return new ModelAndView(new JspView("redirect:/"));
 	}
 
 }
