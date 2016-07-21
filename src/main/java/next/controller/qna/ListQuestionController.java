cdf48532eabcce554ccced478be0b776ec6a185e
youngjun <youngjun@localhost>
2016-07-21 22:10:28 +0900

    9번 질문목록 JSON 조회 구현

--- src/main/java/next/controller/qna/ListQuestionController.java
+++ src/main/java/next/controller/qna/ListQuestionController.java
@@ -5,13 +5,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
+import next.dao.QuestionDao;
 
 public class ListQuestionController extends AbstractController {
-
+	private QuestionDao questionDao = new QuestionDao();
+	
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		ModelAndView mv = jsonView();
+		mv.addObject("questions", questionDao.findAll());
+		return mv;
 	}
 
 }
