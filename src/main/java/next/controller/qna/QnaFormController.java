ab0721298bc59885d6e4e81f8e78f40cd3b3b931
youngjun <youngjun@192.168.0.198>
2016-07-21 20:46:32 +0900

    질문하기 전 로그인 체크하기

--- src/main/java/next/controller/qna/QnaFormController.java
+++ src/main/java/next/controller/qna/QnaFormController.java
@@ -2,14 +2,20 @@ package next.controller.qna;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import core.mvc.AbstractController;
 import core.mvc.ModelAndView;
+import next.controller.UserSessionUtils;
 
 public class QnaFormController extends AbstractController {
 
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+        if(!UserSessionUtils.isLogined(session)){
+        	return jspView("redirect:/");
+        }
 		return jspView("/qna/form.jsp");
 	}
 
