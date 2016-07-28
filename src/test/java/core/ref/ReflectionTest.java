080b607dd30181068c40fcf05fecfdc25be5b407
youngjun <youngjun@192.168.0.21>
2016-07-28 21:51:34 +0900

    RelectionTest 구현

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -1,5 +1,9 @@
 package core.ref;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +17,20 @@ public class ReflectionTest {
 	public void showClass() {
 		Class<Question> clazz = Question.class;
 		logger.debug(clazz.getName());
+		
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			logger.debug("field : {}", field.getName());
+		}
+		
+		Constructor[] constructors = clazz.getDeclaredConstructors();
+		for (Constructor constructor : constructors) {
+			logger.debug("constructor : {}", constructor.getName());
+		}
+		
+		Method[] methods = clazz.getDeclaredMethods();
+		for (Method method : methods) {
+			logger.debug("method : {}", method.getName());
+		}
 	}
 }
