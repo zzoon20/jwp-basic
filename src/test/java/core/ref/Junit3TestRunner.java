06462fb27e4ed24c07f2a88a5fdec2bc03861dbd
youngjun <youngjun@192.168.0.21>
2016-07-28 22:01:39 +0900

    Junit3Test 구현

--- src/test/java/core/ref/Junit3TestRunner.java
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -1,11 +1,19 @@
 package core.ref;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class Junit3TestRunner {
 	@Test
 	public void run() throws Exception {
 		Class<Junit3Test> clazz = Junit3Test.class;
+		Method[] methods = clazz.getMethods();
+		for(Method method : methods){
+			if(method.getName().startsWith("test")){
+				method.invoke(clazz.newInstance());
+			}
+		}
 
 	}
 }
