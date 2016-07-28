d6fd49d2417d0e8d1c39910867704b6b0f88375f
youngjun <youngjun@192.168.0.21>
2016-07-28 22:05:24 +0900

    Junit4Test 구현

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,18 @@
 package core.ref;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class Junit4TestRunner {
 	@Test
 	public void run() throws Exception {
 		Class<Junit4Test> clazz = Junit4Test.class;
-
+		Method[] methods = clazz.getMethods();
+		for(Method method : methods){
+			if(method.isAnnotationPresent(MyTest.class)){
+				method.invoke(clazz.newInstance());
+			}
+		}
 	}
 }
