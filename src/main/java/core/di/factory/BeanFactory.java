4921985eea4461a68dea893668305d2f943f1077
zzoon20 <zzoon20@10.37.129.2>
2016-08-05 17:13:47 +0900

    initialize() 빈 재귀생성 준비

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,5 +1,6 @@
 package core.di.factory;
 
+import java.lang.reflect.Constructor;
 import java.util.Map;
 import java.util.Set;
 
@@ -10,7 +11,7 @@ import com.google.common.collect.Maps;
 
 public class BeanFactory {
 	private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
-	
+
 	private Set<Class<?>> preInstanticateBeans;
 
 	private Map<Class<?>, Object> beans = Maps.newHashMap();
@@ -18,13 +19,26 @@ public class BeanFactory {
 	public BeanFactory(Set<Class<?>> preInstanticateBeans) {
 		this.preInstanticateBeans = preInstanticateBeans;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public <T> T getBean(Class<T> requiredType) {
-		return (T)beans.get(requiredType);
+		return (T) beans.get(requiredType);
 	}
-	
+
 	public void initialize() {
-		
+		for (Class<?> clazz : preInstanticateBeans) {
+			Object obj = getBean(clazz);
+			if (obj == null) {
+				beans.put(clazz, instantiateClass(clazz));
+			}
+		}
+	}
+
+	private Object instantiateClass(Class<?> clazz) {
+		return null;
+	}
+
+	private Object instantiateConstructor(Constructor<?> constructor) {
+		return null;
 	}
 }
